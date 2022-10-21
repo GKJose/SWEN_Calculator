b84605be235b6da4932558622527d15bd936f6bf
GKJose <mendezj8121@uhcl.edu>
2022-10-21 06:08:17 +0000

    fixed errors

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -68,14 +68,13 @@ static void Calculator::input_ta_event_handler(lv_event_t* e)
 	}
 	#if ENABLE_MCP_KEYPAD
    	if(code == LV_EVENT_KEY_PRESSED){
-	   int* btnID = (int *)lv_event_get_user_data(e);
+	   auto btnID = (int *)lv_event_get_user_data(e);
 	   cout << *btnID << endl;
 	   if(*btnID == x_BUTTON){
 		   lv_textarea_add_text(ta,"x");
 	   }else if(*btnID == ENTER_BUTTON){
 		   lv_event_send(ta,LV_EVENT_READY,nullptr);
 	   }
-	   delete btnID;
    }
    #endif
     if (code == LV_EVENT_READY)
