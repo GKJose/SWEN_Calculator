1c5a0f155e3ec0239d8809ca45e2fbc10dc604d6
GKJose <mendezj8121@uhcl.edu>
2022-10-21 05:59:26 +0000

    fixed errors

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -30,7 +30,7 @@ void Calculator::update(lv_timer_t * timer){
 
 	for(int btnID = x_BUTTON; btnID <= ENTER_BUTTON;btnID++){
 		if(keypad.isPressed(btnID)){
-			lv_event_send_recursive(lv_scr_act(),LV_EVENT_KEY_PRESSED,new int(btnID));
+			lv_event_send_recursive(lv_scr_act(),LV_EVENT_KEY_PRESSED,&btnID;
 		}
 	}	
 	#endif
