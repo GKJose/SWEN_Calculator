174c1d8ca2762f170ea67dcb75e7634cd54e35f4
GKJose <mendezj8121@uhcl.edu>
2022-10-21 07:48:34 +0000

    fixed errors

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -73,7 +73,7 @@ static void Calculator::input_ta_event_handler(lv_event_t* e)
 	}
 	#if ENABLE_MCP_KEYPAD
    	if(code == LV_EVENT_KEY_PRESSED){
-	   auto ptr = lv_event_get_user_data(e);
+	   void* ptr = lv_event_get_user_data(e);
 	   cout << "bottom: " << ptr << endl;
 	   return;
 
