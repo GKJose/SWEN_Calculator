1ef32b6d32f00f9e4ca8d410646fe77d61f2ae9c
GKJose <mendezj8121@uhcl.edu>
2022-10-21 01:37:47 +0000

    fixed errors

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -13,6 +13,7 @@ using namespace std;
 static lv_obj_t* textArea,*outputTextArea;
 #if ENABLE_MCP_KEYPAD
 static Keypad keypad;
+int btnID;
 #endif
 
 
@@ -26,7 +27,7 @@ void Calculator::createDemo(){
 void Calculator::update(lv_timer_t * timer){
 	#if ENABLE_MCP_KEYPAD 
 	keypad.poll();
-	for(int btnID = x_BUTTON; btnID <= ENTER_BUTTON; btnID++){
+	for(btnID = x_BUTTON; btnID <= ENTER_BUTTON; btnID++){
 		if(keypad.isPressed(btnID)){
 			lv_event_send_recursive(lv_scr_act(),LV_EVENT_KEY_PRESSED,&btnID);
 		}
