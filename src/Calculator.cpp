8881ab5680adcfb3f5e5db5423dc30c53835d617
GKJose <mendezj8121@uhcl.edu>
2022-10-21 06:49:17 +0000

    fixed errors

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -27,10 +27,10 @@ void Calculator::createDemo(){
 void Calculator::update(lv_timer_t * timer){
 	#if ENABLE_MCP_KEYPAD 
 	keypad.poll();
-	static int btnID;
-	for(btnID = x_BUTTON; btnID <= ENTER_BUTTON;btnID++){
-		if(keypad.isPressed(btnID)){
-			lv_event_send_recursive(lv_scr_act(),LV_EVENT_KEY_PRESSED,&btnID);
+	static int* btnID = lv_mem_alloc(sizeof(int));
+	for(*btnID = x_BUTTON; btnID <= ENTER_BUTTON;btnID++){
+		if(keypad.isPressed(*btnID)){
+			lv_event_send_recursive(lv_scr_act(),LV_EVENT_KEY_PRESSED,btnID);
 		}
 	}	
 
